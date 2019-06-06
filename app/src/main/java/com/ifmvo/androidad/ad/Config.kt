bb5f7bae143f35e4b4e8a7f1b37ec75565b43a2c
Matthew_Chen <Matthew_Chen_1994@163.com>
2019-06-06 10:16:27 +0800

    播放暂停插屏点击其他区域消失

--- app/src/main/java/com/ifmvo/androidad/ad/Config.kt
+++ app/src/main/java/com/ifmvo/androidad/ad/Config.kt
@@ -7,16 +7,16 @@ package com.ifmvo.androidad.ad
  */
 object Config {
 
-    fun splashAdConfig() = "google_admob:0,facebook:1"
+    fun splashAdConfig() = "google_admob:1,facebook:0"
 
-    fun bannerAdConfig() = "google_admob:0,facebook:1"
+    fun bannerAdConfig() = "google_admob:1,facebook:0"
 
-    fun flowAdConfig() = "google_admob:0,facebook:1"
+    fun flowAdConfig() = "google_admob:1,facebook:0"
 
-    fun interAdConfig() = "google_admob:0,facebook:1"
+    fun interAdConfig() = "google_admob:1,facebook:0"
 
-    fun pauseAdConfig() = "google_admob:0,facebook:1"
+    fun pauseAdConfig() = "google_admob:1,facebook:0"
 
-    fun rewardAdConfig() = "google_admob:1,facebook:1"
+    fun rewardAdConfig() = "google_admob:1,facebook:0"
 
 }
