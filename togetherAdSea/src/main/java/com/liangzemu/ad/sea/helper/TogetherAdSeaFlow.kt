c62d20b3b309a4b25cbdf392f5c47151f64185c0
Matthew_Chen <Matthew_Chen_1994@163.com>
2019-06-14 16:19:35 +0800

    flow

--- togetherAdSea/src/main/java/com/liangzemu/ad/sea/helper/TogetherAdSeaFlow.kt
+++ togetherAdSea/src/main/java/com/liangzemu/ad/sea/helper/TogetherAdSeaFlow.kt
@@ -29,92 +29,32 @@ import com.liangzemu.ad.sea.other.loge
  *
  * Created by Matthew_Chen on 2019-04-22.
  */
+/*
+ * (●ﾟωﾟ●) 开屏广告
+ *
+ * Created by Matthew_Chen on 2019-04-22.
+ */
 object TogetherAdSeaFlow : AdBase {
 
-    /**
-     * 横向
-     */
-    fun showAdFlowHorizontal(
+    fun showAdFlow(
         @NonNull context: Context,
         splashConfigStr: String?,
         @NonNull adConstStr: String,
-        @NonNull adListener: TogetherAdSeaFlow.AdListenerFlow
-    ) {
-        //取最高等级
-        val levelCount = Math.max(
-            TogetherAdSea.idListGoogleMap[adConstStr]?.size ?: 0,
-            TogetherAdSea.idListFacebookMap[adConstStr]?.size ?: 0
-        )
-        var level = 0
-        loge("total level:$levelCount level:$level start")
-        //循环等级请求
-        fun showAdFlowByLevel() {
-
-            showAdFlowVertical(
-                context,
-                splashConfigStr,
-                adConstStr,
-                object : TogetherAdSeaFlow.AdListenerFlow {
-                    override fun onStartRequest(channel: String) {
-                        adListener.onStartRequest(channel)
-                    }
-
-                    override fun onAdClick(channel: String) {
-                        adListener.onAdClick(channel)
-                    }
-
-                    override fun onAdFailed(failedMsg: String?) {
-                        loge("level:$level failed:$failedMsg")
-                        if (level >= levelCount) {
-                            adListener.onAdFailed(failedMsg)
-                        } else {
-                            level++
-                            showAdFlowByLevel()
-                        }
-
-                    }
-
-                    override fun onAdPrepared(channel: String, ad: Any) {
-                        loge("level:$level success:$channel")
-                        adListener.onAdPrepared(channel, ad)
-                    }
-
-                },
-                level
-            )
-
-        }
-        //开始请求
-        showAdFlowByLevel()
-    }
-
-    /**
-     * 竖向
-     */
-    fun showAdFlowVertical(
-        @NonNull context: Context,
-        splashConfigStr: String?,
-        @NonNull adConstStr: String,
-        @NonNull adListener: AdListenerFlow,
-        level: Int = -1
+        @NonNull adListener: AdListenerFlow
     ) {
 
         val randomAdName = AdRandomUtil.getRandomAdName(splashConfigStr)
         when (randomAdName) {
             AdNameType.GOOGLE_ADMOB -> showAdFlowGoogle(
-                level,
                 context.applicationContext,
                 splashConfigStr,
                 adConstStr,
-                0,
                 adListener
             )
             AdNameType.FACEBOOK -> showAdFlowFacebook(
-                level,
                 context.applicationContext,
                 splashConfigStr,
                 adConstStr,
-                0,
                 adListener
             )
             else -> {
@@ -130,40 +70,13 @@ object TogetherAdSeaFlow : AdBase {
      * adConstStr : 例：TogetherAdConst.AD_SPLASH
      */
     private fun showAdFlowGoogle(
-        level: Int,
         @NonNull context: Context,
         splashConfigStr: String?,
         @NonNull adConstStr: String,
-        @NonNull requestIndex: Int,
         @NonNull adListener: AdListenerFlow
     ) {
-        /**
-         * 分档检测
-         */
-        val idList =
-            if (level != -1) {
-                TogetherAdSea.idListGoogleMap[adConstStr]?.filterIndexed { index, _ ->
-                    level == index
-                }
-            } else {
-                TogetherAdSea.idListGoogleMap[adConstStr]
-            }
-        //分档位请求完毕  都没广告
-        if (requestIndex >= idList?.size ?: 0) {
-            //如果所有档位都请求失败了，就切换另外一种广告
-            val newConfig = splashConfigStr?.replace(AdNameType.GOOGLE_ADMOB.type, AdNameType.NO.type)
-            showAdFlowVertical(context, newConfig, adConstStr, adListener, level)
-            return
-        }
-        //检测结束 开始请求
-        if (idList.isNullOrEmpty()) {
-            //如果在 Map 里面获取不到该广告位的 idList 意味着初始化的时候没有设置这个广告位
-            loge("${AdNameType.GOOGLE_ADMOB.type}: ${context.getString(R.string.ad_id_no)}")
-            adListener.onAdFailed(context.getString(R.string.ad_id_no))
-            return
-        }
         adListener.onStartRequest(AdNameType.GOOGLE_ADMOB.type)
-        val adLoader = AdLoader.Builder(context, idList[requestIndex])
+        val adLoader = AdLoader.Builder(context, TogetherAdSea.idMapGoogle[adConstStr])
             .forUnifiedNativeAd { ad: UnifiedNativeAd ->
                 logd("${AdNameType.GOOGLE_ADMOB.type}: ${context.getString(R.string.prepared)}")
                 adListener.onAdPrepared(AdNameType.GOOGLE_ADMOB.type, ad)
@@ -171,7 +84,8 @@ object TogetherAdSeaFlow : AdBase {
             .withAdListener(object : AdListener() {
                 override fun onAdFailedToLoad(errorCode: Int) {
                     loge("${AdNameType.GOOGLE_ADMOB.type}: errorCode:$errorCode")
-                    showAdFlowGoogle(level, context, splashConfigStr, adConstStr, requestIndex + 1, adListener)
+                    val newSplashConfig = splashConfigStr?.replace(AdNameType.GOOGLE_ADMOB.type, AdNameType.NO.type)
+                    showAdFlow(context, newSplashConfig, adConstStr, adListener)
                 }
 
                 override fun onAdImpression() {
@@ -198,41 +112,15 @@ object TogetherAdSeaFlow : AdBase {
      * Facebook
      */
     private fun showAdFlowFacebook(
-        level: Int = -1,
         @NonNull context: Context,
         splashConfigStr: String?,
         @NonNull adConstStr: String,
-        @NonNull requestIndex: Int,
         @NonNull adListener: AdListenerFlow
     ) {
-        /**
-         * 分档检测
-         */
-        val idList =
-            if (level != -1) {
-                TogetherAdSea.idListFacebookMap[adConstStr]?.filterIndexed { index, _ ->
-                    level == index
-                }
-            } else {
-                TogetherAdSea.idListFacebookMap[adConstStr]
-            }
-        //分档位请求完毕  都没广告
-        if (requestIndex >= idList?.size ?: 0) {
-            //如果所有档位都请求失败了，就切换另外一种广告
-            val newConfig = splashConfigStr?.replace(AdNameType.FACEBOOK.type, AdNameType.NO.type)
-            showAdFlowVertical(context, newConfig, adConstStr, adListener, level)
-            return
-        }
-        //检测结束 开始请求
-        if (idList.isNullOrEmpty()) {
-            //如果在 Map 里面获取不到该广告位的 idList 意味着初始化的时候没有设置这个广告位
-            loge("${AdNameType.FACEBOOK.type}: ${context.getString(R.string.ad_id_no)}")
-            adListener.onAdFailed(context.getString(R.string.ad_id_no))
-            return
-        }
+
         adListener.onStartRequest(AdNameType.FACEBOOK.type)
 
-        val nativeAd = NativeAd(context, idList[requestIndex])
+        val nativeAd = NativeAd(context, TogetherAdSea.idMapFacebook[adConstStr])
         nativeAd.setAdListener(object : NativeAdListener {
             override fun onAdClicked(ad: Ad?) {
                 logd("${AdNameType.FACEBOOK.type}: ${context.getString(R.string.clicked)}")
@@ -243,13 +131,15 @@ object TogetherAdSeaFlow : AdBase {
 
             override fun onError(ad: Ad?, adError: AdError?) {
                 loge("${AdNameType.FACEBOOK.type}: adError:${adError?.errorCode},${adError?.errorMessage}")
-                showAdFlowFacebook(level, context, splashConfigStr, adConstStr, requestIndex + 1, adListener)
+                val newSplashConfig = splashConfigStr?.replace(AdNameType.FACEBOOK.type, AdNameType.NO.type)
+                showAdFlow(context, newSplashConfig, adConstStr, adListener)
             }
 
             override fun onAdLoaded(ad: Ad?) {
                 if (nativeAd != ad) {
                     loge("${AdNameType.FACEBOOK.type}: 广告返回错误 nativeAd != ad")
-                    showAdFlowFacebook(level, context, splashConfigStr, adConstStr, requestIndex + 1, adListener)
+                    val newSplashConfig = splashConfigStr?.replace(AdNameType.FACEBOOK.type, AdNameType.NO.type)
+                    showAdFlow(context, newSplashConfig, adConstStr, adListener)
                     return
                 }
                 logd("${AdNameType.FACEBOOK.type}: ${context.getString(R.string.prepared)}")
