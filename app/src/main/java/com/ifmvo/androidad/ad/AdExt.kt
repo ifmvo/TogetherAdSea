51df7888870dc9d57a0becf730a9f7ca09eb22ee
Matthew_Chen <Matthew_Chen_1994@163.com>
2019-07-26 15:52:38 +0800

    nothing

--- app/src/main/java/com/ifmvo/androidad/ad/AdExt.kt
+++ app/src/main/java/com/ifmvo/androidad/ad/AdExt.kt
@@ -1,7 +1,6 @@
 package com.ifmvo.androidad.ad
 
 import android.util.Log
-import com.ifmvo.androidad.BuildConfig
 
 /* 
  * (●ﾟωﾟ●)
@@ -9,11 +8,9 @@ import com.ifmvo.androidad.BuildConfig
  * Created by Matthew_Chen on 2019-06-26.
  */
 fun Any.loge(tag: String, msg: String) {
-    if (BuildConfig.DEBUG)
         Log.e(tag, msg)
 }
 
 fun Any.logd(tag: String, msg: String) {
-    if (BuildConfig.DEBUG)
         Log.d(tag, msg)
 }
